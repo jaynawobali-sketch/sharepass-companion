5986836c0f273701ba995a49b11e4e2305d9b461
JayNaw <obalijcharles@gmail.com>
2026-04-25 15:32:24 +0300

    commit

--- .next-dev/static/chunks/webpack.js
+++ .next-dev/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3e9c6cde0996ab3"; }
+/******/ 		__webpack_require__.h = function() { return "2f0d65ee63a05d86"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
